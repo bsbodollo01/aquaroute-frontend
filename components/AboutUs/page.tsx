1b193ffa7db6bcdd785b7d8e0a8e485b202059f2
Bryner Bodollo <bsbodollo01@gmail.com>
2026-01-29 12:24:33 +0800

    chore: updated components for about us page

---
+++ components/AboutUs/page.tsx
@@ -0,0 +1,18 @@
+export default function AboutUsPage() {
+    return (
+        <section id="about" className="py-20 bg-white">
+            <div className="max-w-7xl mx-auto px-6">
+                <h2 className="text-4xl font-bold mb-8 text-center text-balance">About AquaRoute</h2>
+                <p className="text-lg text-gray-700 max-w-3xl mx-auto text-center">
+                    Our platform connects customers with nearby water refilling stations, making clean and affordable water more accessible than ever. 
+                    Instead of searching, calling, or lining up, users can quickly locate trusted stations in their area, compare options, and place orders with ease.
+                </p>
+                <br />
+                <p className="text-lg text-gray-700 max-w-3xl mx-auto text-center">
+                    Built for convenience and reliability, the system helps refilling stations reach more customers while giving users a seamless ordering experience—from discovery to delivery. 
+                    Whether for home or business use, we make staying hydrated simple, efficient, and stress-free.                
+                </p>
+            </div>
+        </section>
+    )
+}
